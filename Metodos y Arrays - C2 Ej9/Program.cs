4398cbccce28b439189d8e2b8c7c50d642cbb2a0
Agneta <agnetagsu@yahoo.es>
2021-05-23 12:04:05 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] miarray = new int[10];
+
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                
+                Console.WriteLine("Escribe el valor a introducir en la posición {0}",i);
+                miarray[i] = Convert.ToInt32(Console.ReadLine()) ;
+            }
+            for (int i = 0; i < miarray.Length; i++)
+            {
+                Console.WriteLine(miarray[i]);
+            }
+        }
+    }
+}
